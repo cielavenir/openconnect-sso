c8f66aa300f11743955e8510bdd16b200981657f
cielavenir <cielartisan@gmail.com>
2025-06-17 01:45:59 +0900

    Revert "Upgrade to PyQt6"
    This reverts commit 829bf747322e4c8147368b7b93b3da7eccbc01ba.

--- openconnect_sso/browser/webengine_process.py
+++ openconnect_sso/browser/webengine_process.py
@@ -9,11 +9,15 @@ import attr
 import pkg_resources
 import structlog
 
-from PyQt6.QtCore import QUrl, QTimer, pyqtSlot, Qt
-from PyQt6.QtNetwork import QNetworkCookie, QNetworkProxy
-from PyQt6.QtWebEngineCore import QWebEngineScript, QWebEngineProfile, QWebEnginePage
-from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtWidgets import QApplication, QWidget, QSizePolicy, QVBoxLayout
+from PyQt5.QtCore import QUrl, QTimer, pyqtSlot, Qt
+from PyQt5.QtNetwork import QNetworkCookie, QNetworkProxy
+from PyQt5.QtWebEngineWidgets import (
+    QWebEngineView,
+    QWebEngineScript,
+    QWebEngineProfile,
+    QWebEnginePage,
+)
+from PyQt5.QtWidgets import QApplication, QWidget, QSizePolicy, QVBoxLayout
 
 from openconnect_sso import config
 
@@ -112,7 +116,7 @@ class Process(multiprocessing.Process):
         web.authenticate_at(QUrl(startup_info.url), startup_info.credentials)
 
         web.show()
-        rc = app.exec()
+        rc = app.exec_()
 
         logger.info("Exiting browser")
         return rc
@@ -160,8 +164,8 @@ class WebBrowser(QWebEngineView):
     def authenticate_at(self, url, credentials):
         script_source = pkg_resources.resource_string(__name__, "user.js").decode()
         script = QWebEngineScript()
-        script.setInjectionPoint(QWebEngineScript.InjectionPoint.DocumentCreation)
-        script.setWorldId(QWebEngineScript.ScriptWorldId.ApplicationWorld)
+        script.setInjectionPoint(QWebEngineScript.DocumentCreation)
+        script.setWorldId(QWebEngineScript.ApplicationWorld)
         script.setSourceCode(script_source)
         self.page().scripts().insert(script)
 
@@ -169,8 +173,8 @@ class WebBrowser(QWebEngineView):
             logger.info("Initiating autologin", cred=credentials)
             for url_pattern, rules in self._auto_fill_rules.items():
                 script = QWebEngineScript()
-                script.setInjectionPoint(QWebEngineScript.InjectionPoint.DocumentReady)
-                script.setWorldId(QWebEngineScript.ScriptWorldId.ApplicationWorld)
+                script.setInjectionPoint(QWebEngineScript.DocumentReady)
+                script.setWorldId(QWebEngineScript.ApplicationWorld)
                 script.setSourceCode(
                     f"""
 // ==UserScript==
